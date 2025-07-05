ca68616f00675a14b8022e6143fd4628db8d1f93
mfraanje <150241076+mfraanje@users.noreply.github.com>
2025-07-05 13:27:35 +0200

    Add test endpoint to isolate pdf-lib issue
    - Tests if simply loading and saving a PDF with pdf-lib causes issues
    - No form operations, just load and save
    - This will help determine if pdf-lib itself is the issue

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ require('./functions/docusignWebhook');
 require('./functions/debugDocuSign');
 require('./functions/testMinimalEnvelope');
 require('./functions/testDirectPdf');
+require('./functions/testPdfFormIssue');
 
 // You can add more function imports here as you create them
 // require('./functions/anotherFunction');
